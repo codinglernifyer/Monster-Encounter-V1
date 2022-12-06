95a0f2e42ae265c5cee3ad8be84c985a519938a6
codinglernifyer <118637176+codinglernifyer@users.noreply.github.com>
2022-12-06 08:57:51 -0800

    Update Monster_Encounter_Main.cpp

--- Monster_Encounter_Main.cpp
+++ Monster_Encounter_Main.cpp
@@ -8,13 +8,26 @@ using namespace std;
 
 
 int main() {
+	// Basic variables for the game.
 	MonsterEncounter MB; // Creates a monster battle object that you can edit attributes of.
 	MB.monsterName = "monster";// Ghange the name of the monster here.
 	MB.startingMonsterHealth = 30;// This is the ammount of health the monster will start with when you start a new game.
 	MB.startingPlayerHealth = 30;// This is the ammount of health the player will start with when you start a new game.
 	MB.maxMoves = 20; // The max ammount of moves before game over will be set here.
-	MB.monsterMove1dmgval = 10; // damage the monster dose to the player with its first move. Default is 10.
-	MB.monsterMove2Regenval = 10 // Health the monster gains using its second move. Default is 10.
+	
+	// Monster move values that controll the power of the monsters diffrent moves.
+	MB.monsterMove1dmgval = 10; // Damage the monster dose to the player with its first move. Default is 10.
+	MB.monsterMove2Regenval = 10; // Health the monster gains using its second move. Default is 10.
+	// The third move dose two things at once.
+	MB.monsterMove3regenval = 4; // Health the monster gains using its third move. Default is 4.
+        MB.monsterMove3dmgval = 4; // Damage the monster dose to the player with its third move. Default is 4.
+	
+	// Player move values that controll the power of the players diffrent moves.
+	MB.playerMove1dmgval = 10; // Damage the player dose to the monster with its first move. Default is 10.
+	MB.playerMove2Regenval = 10; // Health the player gains using its second move. Default is 10.
+	// The third move dose two things at once.
+	MB.playerMove3regenval = 4; // Health the player gains using its third move. Default is 4.
+        MB.playerMove3dmgval = 4; // Damage the player dose to the monster with its third move. Default is 4.
 	
 	
 	MB.MonsterFighterTitleScreen();// This is the void that starts the game.
