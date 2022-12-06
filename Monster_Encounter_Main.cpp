92d34670017bf611dda996f1ad3191cdff8cf234
codinglernifyer <118637176+codinglernifyer@users.noreply.github.com>
2022-12-06 08:52:22 -0800

    Update Monster_Encounter_Main.cpp

--- Monster_Encounter_Main.cpp
+++ Monster_Encounter_Main.cpp
@@ -13,6 +13,8 @@ int main() {
 	MB.startingMonsterHealth = 30;// This is the ammount of health the monster will start with when you start a new game.
 	MB.startingPlayerHealth = 30;// This is the ammount of health the player will start with when you start a new game.
 	MB.maxMoves = 20; // The max ammount of moves before game over will be set here.
+	MB.monsterMove1dmgval = 10; // damage the monster dose to the player with its first move. Default is 10.
+	MB.monsterMove2Regenval = 10 // Health the monster gains using its second move. Default is 10.
 	
 	
 	MB.MonsterFighterTitleScreen();// This is the void that starts the game.
